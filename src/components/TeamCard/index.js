f31d96e6127b39292977656d5febbb70772f08a9
vamshigr <90573265+vamshigr@users.noreply.github.com>
2023-10-28 16:10:55 +0530

    Update index.js

--- src/components/TeamCard/index.js
+++ src/components/TeamCard/index.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 export default function TeamCard({team}) {
   return (
     <>
-    <Link to={`//team-matches/${team.id}`>
+    <Link to={`/team-matches/${team.id}`}>
       <img src={team.team_image_url} />
       <p>{team.name}</p>
   </Link>
