f84509c8fcbb203ac28ba559beda0ec41958fb3b
vamshigr <90573265+vamshigr@users.noreply.github.com>
2023-10-29 20:36:44 +0530

    Update index.js

--- src/components/MatchCard/index.js
+++ src/components/MatchCard/index.js
@@ -2,7 +2,7 @@
 export default function MatchCard({item}) {
   return (
     <>
-      <img src={item.competing_team_logo} />
+      <img src={item.competing_team_logo} alt="my"/>
       <p>{item.competing_team}</p>
       <p>{item.result}</p>
       <p>{item.match_status}</p>
