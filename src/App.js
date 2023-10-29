fcb8880adbddd11ae1dd6c5d88cdd4a79f11cdce
vamshigr <90573265+vamshigr@users.noreply.github.com>
2023-10-29 18:44:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import TeamMatches from './pages/TeamMatches'
 import NotFound from './components/NotFound'
@@ -8,11 +8,11 @@ export default function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/team-matches/:id" element={<TeamMatches />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/team-matches/:id" component={TeamMatches} />
+          <Route component={NotFound} />
+        </Switch>
       </BrowserRouter>
     </>
   )
